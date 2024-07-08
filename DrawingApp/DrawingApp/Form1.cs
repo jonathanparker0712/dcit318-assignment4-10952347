127a7d7964fecf6aef26cca6359006d5e37e1d73
jonathanparker0712 <102540695+jonathanparker0712@users.noreply.github.com>
2024-07-08 18:15:32 +0000

    Add files via upload

---
+++ DrawingApp/DrawingApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Any initialization code can go here
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            lastPoint = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, lastPoint, e.Location);
+                }
+                lastPoint = e.Location;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+    }
+}
+
